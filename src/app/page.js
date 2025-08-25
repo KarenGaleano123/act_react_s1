adf334754eb6d0b4769fc343bf2ca921547745b5
Karen Galeano <karengaleano842@gmail.com>
2025-08-25 16:42:47 -0500

    agregamos y modificamos usuario.jsx y page.js para que se muestren como datos  edad y cuidad

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import Usuario from './components/Usuario';
+
 export default function Home() {
+  const usuario = {
+    nombre: 'Ana García',
+    edad: 25,
+    ciudad: 'Madrid',
+  };
+
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Actividad de Props en Next.js</h1>
+      <Usuario usuario={usuario} />
     </main>
   );
 }
